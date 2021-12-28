13b273c40477554ce42321eed8c877c85d59f0cf
LeBomb <cambon.olivier@gmail.com>
2021-12-28 16:41:30 +0100

    Add light mode management

--- beetleTemp.ino
+++ beetleTemp.ino
@@ -9,11 +9,22 @@ const char hkName[] PROGMEM = "val";
 const char cmdGetName[] PROGMEM = "get";
 const char cmdSetName[] PROGMEM = "set";
 const char tempSensorsName[] PROGMEM = "tempSensors";
+const char lightModeName[] PROGMEM = "lightMode";
 
 OneWire oneWire(A2);
 DallasTemperature tempSensors(&oneWire);
 uint8_t tempSensorsNb = 0;
+#define LIGHT_PIN A1
+uint8_t lightMode = 0;
+uint32_t previousTime_light = 0;
 uint16_t loopNb = 0;
+
+void lightMode_cmdGet(int arg_cnt, char **args) { cnc_print_cmdGet_u32(lightModeName, lightMode); }
+void lightMode_cmdSet(int arg_cnt, char **args) {
+  if(4 == arg_cnt) {
+    lightMode = strtoul(args[3], NULL, 16);
+  }
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -23,11 +34,33 @@ void setup() {
   cnc_cmdGetName_set(cmdGetName);
   cnc_cmdSetName_set(cmdSetName);
   cnc_sepName_set(sepName);
+  cnc_cmdGet_Add(lightModeName, lightMode_cmdGet);
+  cnc_cmdSet_Add(lightModeName, lightMode_cmdSet);
   tempSensorsNb = 0;
   loopNb = 0;
+
+  previousTime_light = millis();
+  pinMode(LIGHT_PIN, OUTPUT);
+  digitalWrite(LIGHT_PIN, HIGH);
+}
+
+void runLight(void) {
+  /* Light blink */
+  uint32_t currentTime_ = 0;
+  currentTime_ = millis();
+  if((uint32_t)(currentTime_ - previousTime_light) >= 100) {
+    if(0 == lightMode) { digitalWrite(LIGHT_PIN, LOW); }
+    else if(1 == lightMode) { digitalWrite(LIGHT_PIN, HIGH); }
+    else if(2 == lightMode) { digitalWrite(LIGHT_PIN, !digitalRead(LIGHT_PIN)); }
+    else { digitalWrite(LIGHT_PIN, LOW); }
+    previousTime_light = currentTime_;
+  }
 }
 
 void loop() {
+  cncPoll();
+  runLight();
+
   if(0 == (loopNb%1000)) {
     tempSensors.begin();
     tempSensorsNb = tempSensors.getDeviceCount();
